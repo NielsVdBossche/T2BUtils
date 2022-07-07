29ee12a5a37025ee5ec343547c1c83d7b268e682
NielsVdBossche <nielsvdbossche@gmail.com>
2022-07-07 11:37:25 +0200

    new script for crontab

---
+++ ArchiveOldOutput.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import glob
+import re
+import os, time, sys
+import shutil
+
+from numpy import full
+
+input = "/user/nivanden/ewkino/_FourTopAnalysis/Output/"
+target = "/pnfs/iihe/cms/store/user/nivanden/AnalysisOutput/TTTT/"
+days = 7
+def getTimestamp(filename):
+    filenameSplit = filename.split("/")[-1]
+    for part in filenameSplit.split('_'):
+        if bool(re.match(r"[0-9]{4}-[0-9]{2}-[0-9]{2}", part)):
+            return part
+
+if __name__ == "__main__":
+    # list all files older than 7 days, take their timestamps and move them to old outputs in a folder named by their timestamp
+    now = time.time()
+    timestampsChecked = []
+    for f in glob.glob(input+"*"):
+        timestamp = getTimestamp(f)
+
+        if timestamp in timestampsChecked: continue
+        timestampsChecked.append(timestamp)
+
+        if os.stat(f).st_mtime > now - days * 86400: continue
+        
+        allStampedFiles = glob.glob(input+"*{}*".format(timestamp))
+
+        fullTarget = target + timestamp + "/"
+        if not os.path.exists( fullTarget ):
+            os.makedirs(fullTarget)
+        
+        for file in allStampedFiles:
+            shutil.move(file, fullTarget)
